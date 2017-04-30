7cd826ec85a361d6e5d51bb8dbc3c0e282979ca7
Matthew Connelly <mattconndev@gmail.com>
2017-04-30 18:19:53 -0400

    Removal of redundant script

--- add-to-package-list.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd ~/Documents/webdev/ubuntu-environment-setup;
-read input;
-echo  $input \\ >> install-packages.sh;
-git add .;
-git commit -m "Addition of $input package.";
-git push;
