87279cf4272d5e539afebe002aece7b901c9e5be
Matthew Connelly <mattconndev@gmail.com>
2017-07-19 19:37:15 -0400

    Addition of thinkpad trackpoint speed-increasing script (x-input)

--- rc-files/.vimrc
+++ rc-files/.vimrc
@@ -3,3 +3,4 @@ noremap <Up> <Nop>
 noremap <Down> <Nop>
 noremap <Left> <Nop>
 noremap <Right> <Nop>
+syntax on
---
+++ scripts/xinput-thinkpad-trackpoint-speed.sh
@@ -0,0 +1,5 @@
+# thinkpad trackpoint speed increase
+# xinput property (by ID): Coordinate Transformation Matrix
+# The last value is the speed, from 0 to 1; the lower the value, the faster the speed.
+# (This script is meant to run on startup)
+xinput --set-prop 12 141 1 0 0 0 1 0 0 0 0.3
