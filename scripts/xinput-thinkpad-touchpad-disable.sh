2642ed465d24e386d14c58f8e65015953904b8c8
Matthew Connelly <mattconndev@gmail.com>
2017-07-20 17:44:44 -0400

    Edit xinput startup scripts to set props based on name (instead of ID which is subject to change seemingly)

---
+++ scripts/xinput-thinkpad-touchpad-disable.sh
@@ -0,0 +1,4 @@
+# Disable touchpad (via property ID)
+# (This script is meant to be run on startup)
+
+xinput --set-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 0
--- scripts/xinput-thinkpad-trackpoint-speed.sh
+++ scripts/xinput-thinkpad-trackpoint-speed.sh
@@ -1,5 +1,4 @@
 # thinkpad trackpoint speed increase
-# xinput property (by ID): Coordinate Transformation Matrix
 # The last value is the speed, from 0 to 1; the lower the value, the faster the speed.
 # (This script is meant to run on startup)
-xinput --set-prop 12 141 1 0 0 0 1 0 0 0 0.3
+xinput --set-prop 12 "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 0.3
