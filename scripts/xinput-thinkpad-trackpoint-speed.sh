899418ac2dca7304222ff8b405ef384d13c96214
Matthew Connelly <mattconndev@gmail.com>
2017-07-20 20:29:56 -0400

    Addition of xfce appfinder script, revision of script naming convention for easy removal, revision of linking script

--- scripts/xinput-thinkpad-trackpoint-speed.sh
+++
@@ -1,4 +0,0 @@
-# thinkpad trackpoint speed increase
-# The last value is the speed, from 0 to 1; the lower the value, the faster the speed.
-# (This script is meant to run on startup)
-xinput --set-prop 12 "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 0.3
